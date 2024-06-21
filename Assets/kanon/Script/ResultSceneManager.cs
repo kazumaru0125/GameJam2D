993500d85d6f4bb1a73c8dc21b5bf2c6e347b502
TSITCL\it232195 <it232195@trident.ac.jp>
2024-06-21 12:21:20 +0900

    TitleSceneとResultSceneの雛形を作りました/Create TitleScene and ResultScene templates.

---
+++ Assets/kanon/Script/ResultSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultSceneManager : MonoBehaviour
+{
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Initiate.Fade("TitleScene", Color.black, 1.0f);
+            //SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
